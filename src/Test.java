fdc30d4090432098211d0cf2c7a83cd7f2c4d921
Lena Palasionak <19lenka87@tut.by>
2022-01-15 13:49:56 +0300

    1.2.3. Changes from Idea

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,8 @@ public class Test {
         System.out.println("1. Commit and push.");
         System.out.println("2. Changes from Idea.");
         System.out.println("3. Changes from Idea.");
-       
-        
+
+
+
     }
 }
