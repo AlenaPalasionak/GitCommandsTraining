e933c8b71ecb3d0da14ed9aa9dcec0eff83fa9ce
Lena Palasionak <19lenka87@tut.by>
2022-01-15 13:36:13 +0300

    3. Changes from Idea

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("1. Commit and push.");
         System.out.println("2. Changes from Idea.");
-        System.out.println("3 Changes from Idea.");
+        System.out.println("3. Changes from Idea.");
     }
 }
