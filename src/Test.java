0dbeabaa42fa71cbaaf47d28971276865b79860f
Lena Palasionak <70293576+AlenaPalasionak@users.noreply.github.com>
2022-01-15 09:38:45 +0300

    Update Test.java
    Changes with mistakes breaking java code convention.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
-public class Test {
-    public static void main(String[] args) {
+public class Test 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("1. Commit and push.");
-    }
-}
+ System.out.println("2. Changes with breaking java code convencion .");  }}
